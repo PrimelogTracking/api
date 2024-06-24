43ef2c37b11ccd7ee0246b39b32f143c6b6e9e4b
PrimelogTracking <primelogtracking@gmail.com>
2024-06-24 16:48:50 -0300

    feat: update cron

--- src/schedules/updateMessages.ts
+++ src/schedules/updateMessages.ts
@@ -3,11 +3,11 @@ import { HistoryController } from "../controllers/history.controller";
 
 const historyController = new HistoryController();
 
-// Agenda a tarefa para rodar a cada 4 dias à meia-noite
-cron.schedule("0 0 */4 * *", async () => {
-  console.log("Atualizando mensagens e títulos...");
+// Agenda a tarefa para rodar todos os dias às 14:00
+cron.schedule("0 14 * * *", async () => {
+  console.log("Atualizando mensagens e títulos às 14:00...");
   await historyController.updateHistoryCode();
-  console.log("Mensagens e títulos atualizados.");
+  console.log("Mensagens e títulos atualizados às 14:00.");
 });
 // "*/10 * * * * *"
 // 0 0 */4 * *
